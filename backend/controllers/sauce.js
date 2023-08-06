16aee81a8d3af3af454eb7561495061a461e7901
Benoît Pntier <46506005+benpntier@users.noreply.github.com>
2023-08-06 15:44:27 +0200

    sauce routes/ctrl reorder

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,6 +1,18 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
 
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error: error }));
+};
+  
+exports.getSauceById = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error: error }));
+};
+
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
@@ -17,18 +29,6 @@ exports.createSauce = (req, res, next) => {
   sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce saved!'}))
     .catch(error => res.status(400).json({ error }));
-};
-
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error: error }));
-};
-
-exports.getSauceById = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-  .then(sauce => res.status(200).json(sauce))
-  .catch(error => res.status(404).json({ error: error }));
 };
 
 exports.modifySauce = (req, res, next) => {
